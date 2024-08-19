1a7a96e8c43b225adc09147570c8f6053f11eb2e
sephsx <olorbidajoseph05@gmail.com>
2024-08-19 08:10:23 +0800

    make some changes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -18,10 +18,25 @@ class ProjectController extends Controller
     public function index()
     {
         $query = Project::query();
-        $projects = $query->paginate(10)->onEachSide(1);
+
+        $sortField = request("sort_field", 'created_at');
+        $sortDirection = request("sort_direction", "desc");
+
+        if (request("name")) {
+            $query->where("name", "like", "%" . request("name") . "%");
+        }
+        if (request("status")) {
+            $query->where("status", request("status"));
+        }
+
+        $projects = $query->orderBy($sortField, $sortDirection)
+            ->paginate(10)
+            ->onEachSide(1);
 
         return inertia("Project/Index", [
-            'projects' => ProjectResource::collection($projects),
+            "projects" => ProjectResource::collection($projects),
+            'queryParams' => request()->query() ?: null,
+            'success' => session('success'),
         ]);
     }
 
@@ -55,7 +70,7 @@ class ProjectController extends Controller
     public function edit(Project $project)
     {
         return Inertia::render('Project/Edit', [
-           
+
         ]);
     }
 
@@ -72,6 +87,6 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        
+
     }
 }
