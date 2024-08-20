bfa496d678f0610356f4a956576cb0bece00ae85
sephsx <olorbidajoseph05@gmail.com>
2024-08-20 22:00:25 +0800

    Update TableHeading.jsx

--- resources/js/Components/TableHeading.jsx
+++ resources/js/Components/TableHeading.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
 
-const TableHeading = () => {
+const TableHeading = ({
+  sort_field = null, 
+  sort_direction = null, 
+  children, 
+  name, 
+  sortable = true,
+  sortChanged = () => {}}
+) => {
   return (
-    <div>TableHeading</div>
+    <th onClick={() => sortChanged(name)}>
+      <div className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider flex items-center justify-between gap-1 cursor-pointer'>
+        {children}
+        {sortable && (
+          <div>
+            <ChevronUpIcon
+              className={`w-4${sort_field === name && sort_direction === 'asc' ? ' text-red-600' : ''}`}
+            />
+            <ChevronDownIcon
+              className={`w-4 -mt-2${sort_field === name && sort_direction === 'desc' ? ' text-red-600' : ''}`}
+            />
+          </div>
+        )}
+      </div>
+    </th>
   )
 }
 
 export default TableHeading
+
+
+
