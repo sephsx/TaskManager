d3ea43ce6cc004fe47e2f87199f0678fc5efbbd1
sephsx <olorbidajoseph05@gmail.com>
2024-08-20 22:00:17 +0800

    Update TaskResource.php

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TaskResource extends JsonResource
@@ -14,6 +16,19 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'image_path' => $this->image_path,
+            'project' => new ProjectResource($this->project),
+            'assignedUser' => $this->assignedUser ? new UserResource($this->assignedUser) : null,
+            'createdBy' => new UserResource($this->createdBy),
+            'updatedBy' => new UserResource($this->updatedBy),
+        ];
     }
 }
